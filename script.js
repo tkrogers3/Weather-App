72dd7738a1d16d1810625ab9807182907f3e76a1
tkrogers3 <tkrogers3@gmail.com>
2020-03-05 18:16:53 -0500

    last update for demo day.

--- script.js
+++ script.js
@@ -1,75 +1,61 @@
 //setting variables and their ID handlers
-
-
-var api= "http://api.openweathermap.org/data/2.5/weather?zip=";
+var api = "http://api.openweathermap.org/data/2.5/weather?zip=";
 var apiKey = ",&appid=85fbdf671b1a59edaae1f9e998fdcbad";
 var btn = document.getElementById("btnSearch");
-var cityName= document.getElementById("cityName");
-var k= document.getElementById("kelvin");
-var f= document.getElementById("fahrenheit");
+var cityName = document.getElementById("cityName");
+var k = document.getElementById("kelvin");
+var f = document.getElementById("fahrenheit");
 var c = document.getElementById("celsius");
-var wthCon= document.getElementById("con");
-var iconImg=document.getElementById("icon");
-var zipCode= document.getElementById("zipInfo"); 
-var alert=document.getElementById("alert");
-var cards=document.getElementById("card-deck");
-
+var wthCon = document.getElementById("con");
+var iconImg = document.getElementById("icon");
+var zipCode = document.getElementById("zipInfo");
+var alert = document.getElementById("alert");
+var cards = document.getElementById("card-deck");
 document.getElementById('btnSearch').addEventListener('click', getWeather);
 
+/* Preventative coding - Not allowing letters or  "+"" | "-" |"e" characters as a part of 
+this statement.*/
+var z = zipCode.length;
+if (z == 5 && (!isNaN(z)));
 
-// Get the button, and when the user clicks on it, execute myFunction
-
-
-/*Function to take zip code info and pull info from 
+/*Function to take zip code info from user and pull info from 
 JSON data received from API Call. If valid, get weather info.
- If invalid, get error message. (I haven't defined catch error yet.) */
+ If invalid, get error message. */
+function getWeather() {
+    fetch(api + zipCode.value + apiKey)    // Fetch = Promise
+        .then((response) => {
+            if (!response.ok) {
+                throw response
+            } return response.json();
 
- var z =zipCode.length;
+        })
+        /*once promise has been met, then data is processed and presented to screen.
+        k and f strings temperate literal to embed expression. Math.floor to round 
+        down to closest integer.*/
 
- if(z== 5 &&(!isNaN(z))); /* Cant figure out how to remove - , + and e
-  //from the allowed characters as a part of this statement. */
+        .then((data) => {   //Promise fulfilled 
 
-  function getWeather () {
-      cardDeck.className="d-block";
-      console.log(zipCode.value);
-    fetch(api + zipCode.value + apiKey)
-    .then((response) => {
-        if(!response.ok){
-            throw response
-        }
-        return response.json();
+            var info = data;
+            cityName.innerHTML = data.name;
+            k.innerHTML = `${Math.floor(info.main.temp)}<sup>o</sup>K `;
+            f.innerHTML = `${Math.floor(((info.main.temp - 273.15) * 9 / 5 + 32))}<sup>o</sup>F`;
+            c.innerHTML = `${Math.floor(info.main.temp - 273.15)}<sup>o</sup>C`;
+            wthCon.innerHTML = info.weather[0].main;
 
-    })
-    /*once promise has been met, then data is processed and presented to screen.
-    k and f strings temperate literal to embed expression. Math.floor to round 
-    down to closest integer.*/
-    
-    .then((data) => {
+            //Template Literal that concatenates the link with the icon to display image  into png.
 
-        var info =data;
-        cityName.innerHTML=data.name;
-        k.innerHTML=`${Math.floor(info.main.temp)}<sup>o</sup>K `;
-        f.innerHTML=`${Math.floor(((info.main.temp -273.15) * 9/5 + 32)) }<sup>o</sup>F`;
-        c.innerHTML=`${Math.floor(info.main.temp -273.15)}<sup>o</sup>C`;
-        wthCon.innerHTML= info.weather[0].main ;
-        //Template Literal that concatenates the link with the icon to display image  into png.
-
-        iconImg.innerHTML = `<img src ="http://openweathermap.org/img/wn/${info.weather[0].icon}.png">`
-        alert.className="d-none alert-primary ";
-        info.weather[0].icon;
-        console.log(con.innerHTML);
-        console.log(info);
-
+            iconImg.innerHTML = `<img src ="http://openweathermap.org/img/wn/${info.weather[0].icon}.png">`
+            alert.className = "d-none alert-primary ";
+            info.weather[0].icon;
+            })
         
-    })
-    /* Haven't figured catch out yet */
-    .catch((error) => {
-     
-        alert.className="d-block alert-warning ";
-     console.log(error);
-    })
-    
+        .catch((error) => { //Promise rejected 
+            //If error has occurred, alert displays below zip search bar.
+            alert.className = "d-block alert-warning ";
+        })
 }
+
+
 
 
 
